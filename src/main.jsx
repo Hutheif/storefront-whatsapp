3d924cc5b205435dc1181bbd375a954ffc405c97
Hutheifa <ihudheifa87@gmail.com>
2025-10-28 20:04:54 +0300

    fixed lower case

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import AdminPanel from "./AdminPanel.jsx";
+import AdminPanel from "./AdminPanel.jsx"; // ✅ fixed: lowercase name
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
